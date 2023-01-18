ac554a68303c6bc9382422bdc997a5c703fbb9fc
Dennis Castrillon <dennis.castrillon@quind.io>
2023-01-18 08:38:08 -0500

    Se agregan 2 test mas para verificar pruebas

---
+++ cypress/e2e/test4.cy.js
@@ -0,0 +1,33 @@
+import { loginPage } from "../pages/LoginPage"
+import { inventoryPage } from "../pages/InventoryPage"
+
+const tests = require('../fixtures/data-driven/sauceUsers.json')
+
+describe('test 4', () => {
+
+  beforeEach(() => {
+    cy.visit("/")
+
+  })
+
+  tests.forEach(test => {
+
+    it(test.name, () => {
+
+      loginPage.typeUsername(test.username)
+      loginPage.typePassword(test.password)
+      loginPage.clickLogin();
+
+      if(test.name == 'should login to inventory page'){
+
+          inventoryPage.elements.title().should('have.text',test.expected)
+      }
+      else{
+        loginPage.elements.errorMessage().should('have.text',test.expected)
+      }
+
+  });
+
+  }) 
+
+})
